ae0cf2b1a437ad10476b3b4da5541a7a045f3f2e
magic-yang <linux.whu@gmail.com>
2011-12-17 17:23:40 +0800

    First pages commit

--- weibo.js
+++ weibo.js
@@ -1,17 +1,17 @@
 (function (win, doc){
 	var weibo = {
 		init: function(){
-			// var jq = doc.createElement('script');
-			// jq.type = 'text/javascript';
-			// if( doc ) {
-			// 	//doc.body.appendChild(jq);
-			// 	doc.body.appendChild(jq);
-			// 	jq.src = '/home/wallace-young/Code/JS/jquery-1.7.1.min.js';
-			// 	//jq.src = 'http://code.jquery.com/jquery-1.7.1.min.js';
+			var jq = doc.createElement('script');
+			jq.type = 'text/javascript';
+			if( doc ) {
+				//doc.body.appendChild(jq);
+				doc.body.appendChild(jq);
+				jq.src = 'http://code.jquery.com/jquery-1.7.1.min.js';
+				// jq.src = '/home/wallace-young/Code/JS/jquery-1.7.1.min.js';
 				alert("init sucess!!");
-			// } else {
-			// 	alert("I am so sorry, i can not help you. \n init failure!!");
-			// }
+			} else {
+				alert("I am so sorry, i can not help you. \n init failure!!");
+			}
 		},
 		hack : function(){
 			// var weibocontent = $("#pl_content_publisherTop.send_weibo_current.input.top_border.bottom_border textarea").val();
@@ -22,4 +22,5 @@
 	};
 	weibo.init();
 	doc.weiboxx = weibo;
+	alert(jQuery);
 })(window, document);
